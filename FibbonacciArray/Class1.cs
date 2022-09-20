6b98cde47bf6824f03a043424e4667744afa882a
Nicklaus Mccarl <nicklausaaron@aol.com>
2022-09-20 13:51:20 -0400

    Add project files.

---
+++ FibbonacciArray/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Specialized;
+using System.Text.Json.Serialization;
+
+namespace FibbonacciArray
+{
+    public class Class1
+    {
+        private readonly int cont;
+
+        int Fib(int n)
+        {
+            if (n <= 1)
+                return n;
+            return Fib(n-2) + Fib(n-1);
+        }
+        object Main()
+        {
+            int n = 25, i;
+            for (i = 0; i < n; i++)
+                Fib(n);
+            return 0;
+        }
+    }
+}
